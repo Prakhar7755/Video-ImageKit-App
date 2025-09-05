68cce7600af03e5152cd5c9035352b33a12d1a5f
Prakhar7755 <prakhar.5779.aps3@gmail.com>
2025-09-05 17:09:15 +0530

    feat: setup database connection

---
+++ lib/db.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI =
+  process.env.MONGODB_URI!; /* exclamatory mark for typescript check */
+
+if (!MONGODB_URI) {
+  throw new Error("Please define mongo uri in ENV file");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const options = {
+      bufferCommands: true,
+      maxPoolSize: 10,
+    };
+
+    mongoose.connect(MONGODB_URI, options).then(() => mongoose.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (err) {
+    cached.promise = null;
+    throw err;
+  }
+
+  return cached.conn;
+}
---
+++ types.d.ts
@@ -0,0 +1,22 @@
+import { Connection } from "mongoose";
+
+declare global {
+  var mongoose: {
+    conn: Connection | null;
+    promise: Promise<Connection> | null;
+  };
+}
+
+export { };
+
+/* 
+You're using Mongoose in your Next.js app to connect to MongoDB.
+
+To reuse the same DB connection between API routes (and avoid reconnecting every time), developers often attach the connection to global.
+
+But TypeScript doesn’t know that you’ve added mongoose to the global object, so it complains. 😬
+
+So, you create this types.d.ts file to tell TypeScript:
+
+"Hey, there's a new thing on the global object called mongoose, and here’s what it looks like." 
+*/
